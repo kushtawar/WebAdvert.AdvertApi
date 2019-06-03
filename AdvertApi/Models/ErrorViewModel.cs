9ca562309c401b243b79f4f9421958c94dc8c0f5
kushtawar <kushtawar@gmail.com>
2019-06-03 20:22:59 +0200

    AdvertAPI Before CodeDeploy Image

---
+++ AdvertApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdvertApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AdvertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,17 @@
+﻿using AdvertApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+//using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+
+        //Task<AdvertModel> GetByIdAsync(string id);
+        Task<bool> CheckHealthAsync();
+        //Task<List<AdvertModel>> GetAllAsync();
+    }
+}
